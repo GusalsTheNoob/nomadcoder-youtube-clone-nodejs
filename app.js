9fdcdad5b5131fc7dd277d7554b3635a0b8ef1dd
GusalsTheNoob <harrygusals@snu.ac.kr>
2020-12-05 11:09:20 +0900

    2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from home");
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -15,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+app.use("/", globalRouter);
+app.use("/profile", videoRouter);
 app.use("/user", userRouter);
 
 export default app;
